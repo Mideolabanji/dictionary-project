8fda38e26e3e0769deff0721cec81b3ecdb78c58
Mideolabanji <olabanjikaosara@yahoo.com>
2022-02-25 16:27:25 +0100

    included the form input element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./Form";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { InputGroup } from "react-bootstrap";
+
+export default function Form() {
+  return (
+    <div>
+      <form>
+        <input type="search" />
+      </form>
+    </div>
+  );
+}
